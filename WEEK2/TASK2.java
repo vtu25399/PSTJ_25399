ea044595ef29c85cb707bc5952db2019a5d7df7a
vtu25399 <vtu25399@veltech.edu.in>
2026-02-03 11:14:54 +0530

    Update TASK2.java

--- WEEK2/TASK2.java
+++ WEEK2/TASK2.java
@@ -1,46 +1,41 @@
-package PSTJ;
+package Prime;
 
 import java.util.*;
-import java.util.function.*;
 
 interface PerformOperation {
     boolean check(int a);
 }
 
-class MyMath {
+public class Main {
+
     public static PerformOperation isOdd() {
-        return n -> n % 2 != 0;
+        return (int a) -> a % 2 != 0;
     }
+
     public static PerformOperation isPrime() {
-        return n -> {
-            if (n <= 1) return false;
-            for (int i = 2; i <= Math.sqrt(n); i++) {
-                if (n % i == 0)
-                    return false;
+        return (int a) -> {
+            if (a <= 1) return false;
+            for (int i = 2; i <= Math.sqrt(a); i++) {
+                if (a % i == 0) return false;
             }
             return true;
         };
     }
+
     public static PerformOperation isPalindrome() {
-        return n -> {
-            int original = n, reverse = 0;
-            while (n > 0) {
-                reverse = reverse * 10 + n % 10;
-                n /= 10;
+        return (int a) -> {
+            int original = a;
+            int rev = 0;
+            while (a > 0) {
+                rev = rev * 10 + a % 10;
+                a /= 10;
             }
-            return original == reverse;
+            return original == rev;
         };
     }
 
-    public static boolean checker(PerformOperation op, int num) {
-        return op.check(num);
-    }
-}
-
-public class TASK2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         int T = sc.nextInt();
 
         while (T-- > 0) {
@@ -51,18 +46,18 @@ public class TASK2 {
             boolean result;
 
             if (type == 1) {
-                op = MyMath.isOdd();
-                result = MyMath.checker(op, num);
+                op = isOdd();
+                result = op.check(num);
                 System.out.println(result ? "ODD" : "EVEN");
-
-            } else if (type == 2) {
-                op = MyMath.isPrime();
-                result = MyMath.checker(op, num);
+            } 
+            else if (type == 2) {
+                op = isPrime();
+                result = op.check(num);
                 System.out.println(result ? "PRIME" : "COMPOSITE");
-
-            } else if (type == 3) {
-                op = MyMath.isPalindrome();
-                result = MyMath.checker(op, num);
+            } 
+            else if (type == 3) {
+                op = isPalindrome();
+                result = op.check(num);
                 System.out.println(result ? "PALINDROME" : "NOT PALINDROME");
             }
         }
